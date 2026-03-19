c40accef90f93f58262e5ed3bffe275ba6afb031
Rosa <ripy36@gmail.com>
2026-03-19 11:12:26 -0500

    feat: proyecto Spring Boot base

---
+++ backend/proyecto-orquideas/src/main/java/com/orquicombeima/proyecto_orquideas/ProyectoOrquideasApplication.java
@@ -0,0 +1,13 @@
+package com.orquicombeima.proyecto_orquideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOrquideasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOrquideasApplication.class, args);
+	}
+
+}
---
+++ backend/proyecto-orquideas/src/test/java/com/orquicombeima/proyecto_orquideas/ProyectoOrquideasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orquicombeima.proyecto_orquideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoOrquideasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
