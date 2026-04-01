a10d424a59e91e43124d8372d625b5d22e7aef29
JuanseGallego <juansebastiangallegovillamil@gmail.com>
2026-04-01 18:41:17 -0500

    Crear PedidoRepository y corregir un metodo de ReservaCarritoRepository porque habia quedado mal el tipo

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/repository/ReservaCarritoRepository.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/repository/ReservaCarritoRepository.java
@@ -4,10 +4,11 @@ import com.orquicombeima.proyecto_orquideas.model.ReservaCarrito;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReservaCarritoRepository extends JpaRepository<ReservaCarrito, Long> {
     // Metodo para buscar una reserva por el id del carrito
-    Optional<ReservaCarrito> finnByCarritoId(Long carritoId);
+    List<ReservaCarrito> finnByCarritoId(Long carritoId);
 }
