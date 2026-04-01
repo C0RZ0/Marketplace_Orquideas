c7a74ec81206424b59d4325f5988862293a2db9f
Rosa <ripy36@gmail.com>
2026-03-31 22:17:08 -0500

    Agregar comentarios a Orquidea

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/Orquidea.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/Orquidea.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// Orquidea hereda todos los campos de Producto (id, nombre, precio, stock, imageUrl, activo) y agrega sus propios campos específicos
+// En la BD existirá una tabla "orquideas" con solo sus campos propios, conectada a "productos" por el mismo id
 @Entity
 @Table(name = "orquideas")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@PrimaryKeyJoinColumn(name = "id")
-
+@PrimaryKeyJoinColumn(name = "id")          // le dice a JPA que el id de orquideas es FK hacia productos
 
 public class Orquidea extends Producto {
 
