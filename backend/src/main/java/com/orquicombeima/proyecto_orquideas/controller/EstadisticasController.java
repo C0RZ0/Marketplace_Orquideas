059d521cae9e1300cc7d5e16ae7b6f59d8f394f3
JuanseGallego <juansebastiangallegovillamil@gmail.com>
2026-05-07 21:11:39 -0500

    Agregar el endpoint para obtener los 5 pedidos mas recientes

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/controller/EstadisticasController.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/controller/EstadisticasController.java
@@ -1,6 +1,7 @@
 package com.orquicombeima.proyecto_orquideas.controller;
 
 import com.orquicombeima.proyecto_orquideas.dto.EstadisticasDTO;
+import com.orquicombeima.proyecto_orquideas.dto.PedidoRecienteDTO;
 import com.orquicombeima.proyecto_orquideas.service.EstadisticasService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -20,6 +23,11 @@ public class EstadisticasController {
     @GetMapping("/estadisticas")
     public ResponseEntity<EstadisticasDTO> obtenerEstadisticas() {
         return ResponseEntity.ok(estadisticasService.obtenerEstadisticas());
+    }
+
+    @GetMapping("/pedidos/recientes")
+    public ResponseEntity<List<PedidoRecienteDTO>> obtenerPedidosRecientes() {
+        return  ResponseEntity.ok(estadisticasService.obtenerPedidosRecientes());
     }
 
 }
