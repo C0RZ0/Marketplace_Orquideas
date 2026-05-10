7d5fa22a97af0b37293bf803ee54583b5367c7c0
JuanseGallego <juansebastiangallegovillamil@gmail.com>
2026-05-10 18:39:02 -0500

    Crear el DTO para el mensaje del historial del chatbot

---
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/dto/MensajeHistorialDTO.java
@@ -0,0 +1,14 @@
+package com.orquicombeima.proyecto_orquideas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MensajeHistorialDTO {
+
+    private String rol;
+    private String texto;
+}
