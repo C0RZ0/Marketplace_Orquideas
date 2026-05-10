d09a1d0139f9ae56c5ca20171ea25f646d842120
JuanseGallego <juansebastiangallegovillamil@gmail.com>
2026-05-10 00:20:16 -0500

    Agregar el service del chatbot en donde se le da el contexto, se construye el prompt y finalmente se hace el llamado a Gemini con el mensaje que desea el usuario

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/dto/ChatbotResponseDTO.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/dto/ChatbotResponseDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ChatBotResponseDTO {
+public class ChatbotResponseDTO {
 
     private String respuesta;
 }
